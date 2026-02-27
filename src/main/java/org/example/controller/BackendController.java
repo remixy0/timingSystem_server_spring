0a4ac35a18d89eebf7e3e3fef9a70a863db218df
Stanislaw Sikora <remixy0@onet.pl>
2026-02-28 00:21:28 +0100

    update

--- src/main/java/org/example/controller/BackendController.java
+++ src/main/java/org/example/controller/BackendController.java
@@ -4,7 +4,6 @@ import org.example.model.Effort;
 import org.example.model.DTOs.EffortDTO;
 import org.example.service.Service;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
--- src/main/java/org/example/model/DTOs/EffortDTO.java
+++ src/main/java/org/example/model/DTOs/EffortDTO.java
@@ -1,5 +1,4 @@
 package org.example.model.DTOs;
-
 import java.util.List;
 
 public class EffortDTO {
